8b4edc52da02d29ec1354e84fded2efbcdb1b54c
rachitgupta98 <rachit371@gmail.com>
2020-05-17 12:58:50 +0530

    create startGameScreen

---
+++ screens/startGameScreen.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const StartGameScreen = (props) => {
+  return;
+};
+
+const style = StyleSheet.create({});
+export default StartGameScreen;
