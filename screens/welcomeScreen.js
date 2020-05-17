b737389f6c744fd58d35c73fec4fbb12018115c2
rachitgupta98 <rachit371@gmail.com>
2020-05-17 14:18:38 +0530

    modified

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import BTN from "../components/customButton";
 import Card from "../components/customCard";
+import Input from "../components/customInput";
 const WelcomeScreen = (props) => {
+  const [userInput, setUserInput] = useState("");
+  const handleUserInput = (input) => {
+    setUserInput(input.replace(/[^0-9]/g, ""));
+  };
+  const onReset = () => {
+    console.log("rest clicked");
+  };
+  const onConfirm = () => {
+    console.log("Confirm clicked");
+  };
   return (
-    <View style={styles.welcomeScreen}>
-      <Card style={styles.card}>
-        <Text>Welcome Screen</Text>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.welcomeScreen}>
+        <Card style={styles.card}>
+          <Text>Welcome Screen</Text>
 
-        <TextInput />
-        <View style={styles.buttonView}>
-          <BTN style={styles.reset}>RESET</BTN>
-          <BTN style={styles.confirm}>CONFIRM</BTN>
-        </View>
-      </Card>
-    </View>
+          <Input
+            style={styles.input}
+            keyboardType="number-pad"
+            maxLength={2}
+            value={userInput}
+            onChangeText={handleUserInput}
+          />
+          <View style={styles.buttonView}>
+            <BTN style={styles.reset} onBtnPress={onReset}>
+              RESET
+            </BTN>
+            <BTN style={styles.confirm}>CONFIRM</BTN>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -39,6 +66,10 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
   },
 });
 
