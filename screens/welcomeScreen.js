775b0f743085526279c80c7c9cf7c13574915f7d
rachitgupta98 <rachit371@gmail.com>
2020-05-17 13:38:36 +0530

    use card

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 import BTN from "../components/customButton";
+import Card from "../components/customCard";
 const WelcomeScreen = (props) => {
   return (
-    <View style={style.welcomeScreen}>
-      <Text>Welcome Screen</Text>
-      <TextInput />
-      <View style={style.buttonView}>
-        <BTN style={style.reset}>RESET</BTN>
-        <BTN style={style.confirm}>CONFIRM</BTN>
-      </View>
+    <View style={styles.welcomeScreen}>
+      <Card style={styles.card}>
+        <Text>Welcome Screen</Text>
+
+        <TextInput />
+        <View style={styles.buttonView}>
+          <BTN style={styles.reset}>RESET</BTN>
+          <BTN style={styles.confirm}>CONFIRM</BTN>
+        </View>
+      </Card>
     </View>
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   welcomeScreen: {
     flex: 1,
     alignItems: "center",
     //justifyContent: "center",
-    padding: 10,
+    padding: 20,
   },
   reset: {
     color: "red",
@@ -30,6 +34,11 @@ const style = StyleSheet.create({
   buttonView: {
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  card: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
   },
 });
 
