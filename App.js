3f8d3ca0da06004710cf48f505cc9c6a52b8a62e
rachitgupta98 <rachit371@gmail.com>
2020-05-17 12:56:32 +0530

    modified app.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./components/header";
+import WelcomeScreen from "./screens/welcomeScreen";
 export default function App() {
   return (
     <View style={styles.container}>
       <Header title="Guess a Number"></Header>
+      <WelcomeScreen />
     </View>
   );
 }
