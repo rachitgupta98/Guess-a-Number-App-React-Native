a76dfed8850b5cf4f58c8ef3193c49ba13da7d8b
rachitgupta98 <rachit371@gmail.com>
2020-05-17 11:56:01 +0530

    add custom header

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/header";
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header title="Guess a Number"></Header>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
