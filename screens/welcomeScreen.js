aa54edbcbbabde75656f5f8d562ba48c0940f447
rachitgupta98 <rachit371@gmail.com>
2020-05-18 22:17:06 +0530

    alert feature added

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -5,26 +5,45 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from "react-native";
 import BTN from "../components/customButton";
 import Card from "../components/customCard";
 import Input from "../components/customInput";
 const WelcomeScreen = (props) => {
   const [userInput, setUserInput] = useState("");
+  const [confirm, setConfirm] = useState(false);
+  const [finalValue, setFinalValue] = useState();
+  const [chance, setChances] = useState(3);
   const handleUserInput = (input) => {
     setUserInput(input.replace(/[^0-9]/g, ""));
   };
   const onReset = () => {
-    console.log("rest clicked");
+    setUserInput("");
+    setConfirm(false);
   };
   const onConfirm = () => {
-    console.log("Confirm clicked");
+    let chosenNum = parseInt(userInput);
+    if (isNaN(chosenNum) || chosenNum <= 0) {
+      Alert.alert(
+        "Invalid Number",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "OK", style: "destructive", onPress: onReset }]
+      );
+    }
+    setFinalValue(chosenNum);
+    setConfirm(true);
+    setUserInput("");
   };
+  let selectedNumber;
+  if (confirm) {
+    selectedNumber = <Text>You Select : {finalValue}</Text>;
+  }
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.welcomeScreen}>
         <Card style={styles.card}>
-          <Text>Welcome Screen</Text>
+          <Text>Enter a Number</Text>
 
           <Input
             style={styles.input}
@@ -37,9 +56,12 @@ const WelcomeScreen = (props) => {
             <BTN style={styles.reset} onBtnPress={onReset}>
               RESET
             </BTN>
-            <BTN style={styles.confirm}>CONFIRM</BTN>
+            <BTN style={styles.confirm} onBtnPress={onConfirm}>
+              CONFIRM
+            </BTN>
           </View>
         </Card>
+        {selectedNumber}
       </View>
     </TouchableWithoutFeedback>
   );
