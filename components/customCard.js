36d75be1257b929a283d8fb5cabdad073917bfa8
rachitgupta98 <rachit371@gmail.com>
2020-05-18 22:58:41 +0530

    remove elevation

--- components/customCard.js
+++ components/customCard.js
@@ -3,16 +3,19 @@ import { StyleSheet, View } from "react-native";
 
 const Card = (props) => {
   return (
-    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+    <View {...props} style={{ ...styles.card, ...props.style }}>
+      {props.children}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    elevation: 10,
+    //elevation: 10,
     backgroundColor: "white",
     borderRadius: 10,
     padding: 20,
+    marginVertical: 10,
   },
 });
 export default Card;
